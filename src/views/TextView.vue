d3c70182a1a72c3fe4e862ab549fdf2f93eab6bd
Hai_Yao <2019086272@qq.com>
2025-03-05 14:47:52 +0800

    初始项目

---
+++ src/views/TextView.vue
@@ -0,0 +1,15 @@
+<template>
+	<div class="text">
+		<h1>测试界面</h1>
+	</div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+	.text {
+		min-height: 100vh;
+		display: flex;
+		align-items: center;
+	}
+}
+</style>
