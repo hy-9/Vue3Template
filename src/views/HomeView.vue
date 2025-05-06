b1d3987ace8aabb0b559f0450cd5a77ec4288d36
HaiYao <2019086272@qq.com>
2025-05-06 14:31:39 +0800

    添加TailwindCss支持

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div class="text">
-		<h1>测试界面</h1>
+	<div
+		class="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg"
+	>
+		<div class="shrink-0">
+			<img
+				class="size-12"
+				src="..\..\public\favicon.ico"
+				alt="ChitChat Logo"
+			/>
+		</div>
+		<div>
+			<div class="text-xl font-medium text-black">ChitChat</div>
+			<p class="text-slate-500">You have a new message!</p>
+		</div>
 	</div>
 </template>
-
 <style></style>
