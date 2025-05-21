299c192845d8da510f1d9fa2f3497d6d0665c1dc
HaiYao <2019086272@qq.com>
2025-05-21 10:20:18 +0800

    配置文件

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
-import { ref } from "vue";
-const a = ref({ n: 1 });
-const b = ref(1);
-console.log(a);
-console.log(b);
+
+console.log(import.meta.env);
 </script>
 
 <template>
